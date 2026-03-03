bbbb3ed79fd89ae276fa97351b18f0036e74457e
TIGRE9725 <anyelo.roldan.97@gmail.com>
2026-03-02 23:13:08 -0600

    Modify cloudstream configuration for NetVideo
    Updated cloudstream properties to reflect new details and added Spanish language support.

--- NetVideoProvider/build.gradle.kts
+++ NetVideoProvider/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    val implementation by configurations
+    implementation("org.jsoup:jsoup:1.18.3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
@@ -7,27 +9,12 @@ dependencies {
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
+    description = "Películas y Series de NetVideo con Failover" [cite: 5]
+    authors = listOf("NetVideo") // Pon tu nombre aquí [cite: 5]
+    status = 1 
+    tvTypes = listOf("Movie", "TvSeries") // Añadimos TvSeries
     requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    language = "es" // Cambiado a español
 }
 
 android {
